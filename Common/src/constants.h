8b24b2b1b30b33a3ae3ffb1d053c1c0f992490ef
acidicoala <acidicoala@gmail.com>
2021-03-27 10:04:47 +0200

    Disable Family Sharing lock bypass by default

--- Common/src/constants.h
+++ Common/src/constants.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr auto VERSION = "1.2.0";
+constexpr auto VERSION = "1.2.1";
 
 constexpr auto INTEGRATION_64 = L"Integration64.dll";
 constexpr auto INTEGRATION_32 = L"Integration32.dll";
